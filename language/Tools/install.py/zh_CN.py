fd12181d807342501aadb1b6eab72deb72e2a5c3
jiang <1446935823@qq.com>
2023-08-22 04:27:51 +0800

    语言文件

---
+++ language/Tools/install.py/zh_CN.py
@@ -0,0 +1,11 @@
+
+text = {}
+text["name"] = "# JCM (jiang 终端 管理系统) 安装程序#"
+text["insdir"] = "安装目录: "
+text["port"] = "端口: "
+text["boot"] = "自启: "
+text["qr"] = "确认信息,输入exit退出安装"
+text["oserr"] = "获取 OS 名称 出错"
+text["init"] = "初始化"
+text["osname"] = "OS 名称:"
+text["devname"] = "主机名:"
---
+++ language/server/init.py/zh_CN.py
@@ -0,0 +1,7 @@
+
+text = {}
+text["osname"] = "OS 名称:"
+text["devname"] = "主机名:"
+text["init"] = "init"
+text["user"] = "User:"
+text["passwor"] = "Password:"
