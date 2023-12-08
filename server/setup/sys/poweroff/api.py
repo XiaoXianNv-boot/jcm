dd47ed35929b04e0176a55046da0df3aeb6a1df6
XiaoXianNv-boot <qw1446935823@icloud.com>
2023-12-08 11:32:52 +0000

    系统操作API

---
+++ server/setup/sys/poweroff/api.py
@@ -0,0 +1,35 @@
+
+# coding=utf-8
+#!/bin/python
+
+import os
+import sys
+import time
+import imp
+import hashlib
+import socket
+
+def run(info):
+    os.system("sleep 30 && poweroff")
+
+def poweroff(info):
+    t = threading.Thread(target=run, args=(info))
+    t.start()
+    return "30S poweroff"
+
+def main(new_client_socket,post,Headers,info,user):
+    link = ''
+    path = ''
+    res = '{}'
+    for i in post:
+        tmp = i.split('=')
+        if tmp[0] == 'link':
+            link = tmp[1]
+        if tmp[0] == 'path':
+            path = tmp[1]
+
+    res = '{"data":"' + poweroff(info) + '"}'
+
+
+    httpserver = imp.load_source("server/main/httpserver.py","server/main/httpserver.py")
+    httpserver.httppostchar(new_client_socket,"200",res.encode("utf-8"),"application/json",Headers,info)
