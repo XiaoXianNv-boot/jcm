43972d4058811310a82599c5b788dc5f1c6f8a57
XiaoXianNv-boot <76765956+XiaoXianNv-boot@users.noreply.github.com>
2024-02-15 01:25:51 +0000

    添加英语的语言文件

---
+++ language/Tools/install.py/en_US.py
@@ -0,0 +1,11 @@
+
+text = {}
+text["name"] = "# JCM (jiang Cluster Management) Installer#"
+text["insdir"] = "install dir: "
+text["port"] = "port: "
+text["boot"] = "boot: "
+text["qr"] = "Confirm the information, enter the exit exit installation"
+text["oserr"] = "Get OS name error"
+text["init"] = "init"
+text["osname"] = "OS Name:"
+text["devname"] = "Host Name:"
