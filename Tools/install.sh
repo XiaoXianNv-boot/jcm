66528a356c53ecdbf3b49e5716b3da9139be015a
XiaoXianNv-boot <76765956+XiaoXianNv-boot@users.noreply.github.com>
2024-02-15 00:28:03 +0000

    添加镜像源,添加虚拟环境

--- Tools/install.sh
+++ Tools/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export dir=https://github.com/XiaoXianNv-boot/jcm/releases/download/Preview
+export gitdir=https://github.com/XiaoXianNv-boot/jcm/raw/master
+export mirrordir=http://jiang144.i234.me/data/jcm
 
 if [ ! -e /usr/bin/curl ]; then
     if [ -e /bin/apt ]; then
@@ -25,12 +26,15 @@ if [ ! -e /bin/python3 ]; then
     if [ -e /bin/apt ]; then
         apt update
         apt install -y python3
+        apt install -y python3-venv
     else
         if [ -e /bin/yum ]; then
             yum install -y python3
+            yum install -y python3-venv
         else
             if [ -e /sbin/apk ]; then
                 apk add --no-cache python3
+                apk add --no-cache python3-venv
             else
                 if [ -e /bin/opkg ]; then
                     opkg update
@@ -41,20 +45,26 @@ if [ ! -e /bin/python3 ]; then
     fi
 fi
 
+
 mkdir -p jcm_install
 cd jcm_install
 
 mkdir -p Tools
 cd Tools
-curl -#fL -o install.py -C - $dir/install.py
+curl -#fL -o install.py -C - $gitdir/install/install.py
+if [ ! -e install.py ]; then
+    curl -#fL -o install.py -C - $dir/install/install.py
+fi
 cd ..
 mkdir -p lib
 cd lib
 mkdir -p pkg
 cd pkg
-curl -#fL -o APP_V0.2.pkg -C - $dir/APP_V0.2.pkg
-curl -#fL -o main_V0.2.pkg -C - $dir/main_V0.2.pkg
+#curl -#fL -o APP_V0.2.pkg -C - $dir/APP_V0.2.pkg
+#curl -#fL -o main_V0.2.pkg -C - $dir/pkg/main_V0.2.pkg
 cd ..
 cd ..
 
+python3 -m venv venv
+source ./venv/bin/activate
 python3 Tools/install.py %1 %2 %3 %4 %5
